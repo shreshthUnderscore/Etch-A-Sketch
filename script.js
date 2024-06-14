dc0c5f8f7a00d38ded1b66081a599eaa6326d89c
shreshthUnderscore <88784050+shreshthUnderscore@users.noreply.github.com>
2024-06-14 08:33:16 +0530

    [UPDATED] code for the gui and grid implementation

--- script.js
+++ script.js
@@ -1,14 +1,54 @@
+var gridRow = 4;
+
 const containerDivEle = document.querySelector(".containerDiv");
+const sizeButtonEle = document.querySelector(".size-button");
+const resetButtonEle = document.querySelector(".reset-button");
 
-for(var i=0; i < 16; i++)
+function createGrid(size)
 {
-    var div = document.createElement("div");
-    div.setAttribute("class", "gridDiv")
+    containerDivEle.innerHTML = '';
 
-    div.addEventListener("mouseenter", function() {
-        this.classList.add("gridDivHover");
-    });
-
-    containerDivEle.appendChild(div);
+    for(var j=0; j < size; j++)
+    {
+        var newDiv = document.createElement("div");
+        newDiv.setAttribute("class", "newDiv");
+        for(var i=0; i < size; i++)
+        {
+            var div = document.createElement("div");
+            div.setAttribute("class", "gridDiv")
+        
+            div.addEventListener("mouseenter", function() {
+                this.classList.add("gridDivHover");
+            });
+        
+            newDiv.appendChild(div);
+        }
+        containerDivEle.appendChild(newDiv);
+    }    
+    
 }
 
+createGrid(gridRow);
+
+sizeButtonEle.addEventListener("click", function ()
+{
+    gridRow = prompt("Enter Number of Rows/Columns","16");
+
+    if(gridRow !== null)
+    {
+        gridRow = parseInt(gridRow);
+        if(gridRow !== NaN && gridRow > 0)
+        {
+            createGrid(gridRow);
+        }
+        else
+        {
+            alert("Enter a valid number !!");
+        }
+    }
+});
+
+resetButtonEle.addEventListener("click", function ()
+{
+    createGrid(gridRow);
+});
